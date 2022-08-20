ae216f8d7083712a196dba6450df495a060967b1
Shy Alter <puemos@gmail.com>
2022-08-20 23:02:34 +0200

    change: endpoint runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :overbooked, OverbookedWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: host, port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
