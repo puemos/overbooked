6c348d9320d698fe2204e1547f9cccceea71d174
Shy Alter <puemos@gmail.com>
2022-08-20 23:29:51 +0200

    revert: 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Overbooked.MixProject do
     [
       app: :overbooked,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
